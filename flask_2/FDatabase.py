c56946647e5d0e16d36f3a9516f1476782af8a17
v1pung <kaby.spb1@gmail.com>
2024-10-15 20:39:58 +0300

    9

--- flask_2/FDatabase.py
+++ flask_2/FDatabase.py
@@ -1,3 +1,6 @@
+import math
+import time
+import sqlite3
 class FDatabase:
     def __init__(self, db):
         self.__db = db
@@ -13,3 +16,34 @@ class FDatabase:
             print('Error reading from db')
         return []
 
+    def addPost(self, title, text):
+        try:
+            tm = math.floor(time.time())
+            self.__cur.execute("INSERT INTO posts VALUES(NULL, ?, ?, ?)", (title, text, tm))
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print("Ошибка добавления статьи в БД " + str(e))
+            return False
+
+        return True
+
+    def getPost(self, postId):
+        try:
+            self.__cur.execute(f"SELECT title, text FROM posts WHERE id = {postId} LIMIT 1")
+            res = self.__cur.fetchone()
+            if res:
+                return res
+        except sqlite3.Error as e:
+            print("Ошибка получения статьи из БД " + str(e))
+
+        return (False, False)
+
+    def getPostsAnonce(self):
+        try:
+            self.__cur .execute(f"SELECT id, title, text FROM posts ORDER BY time DESC")
+            res = self.__cur.fetchall()
+            if res: return res
+        except sqlite3.Error as e:
+            print("Ошибка получения статьи " + str(e))
+
+        return []
--- flask_2/fl_2.py
+++ flask_2/fl_2.py
@@ -1,6 +1,8 @@
 import os
 import sqlite3
+
 from flask import Flask, render_template, request, g, flash
+
 from FDatabase import FDatabase
 
 # cfg
@@ -45,10 +47,11 @@ def close_db(error):
 def index():
     db = get_db()
     dbase = FDatabase(db)
-    return render_template('index.html', menu=dbase.getMenu())
+    return render_template('index.html', menu=dbase.getMenu(), posts=dbase.getPostsAnonce())
+
 
 @app.route("/add_post", methods=["POST", "GET"])
-def add_post():
+def addPost():
     db = get_db()
     dbase = FDatabase(db)
 
@@ -56,13 +59,24 @@ def add_post():
         if len(request.form['name']) > 4 and len(request.form['post']) > 10:
             res = dbase.addPost(request.form['name'], request.form['post'])
             if not res:
-                flash('Ошибка добавления статьи', category = 'error')
+                flash('Ошибка добавления статьи', category='error')
             else:
-                flash('Статья добавлена успешно', category = 'success')
+                flash('Статья добавлена успешно', category='success')
         else:
-            flash('Ошибка добавления статьи', category = 'error')
+            flash('Ошибка добавления статьи', category='error')
 
     return render_template('add_post.html', menu=dbase.getMenu(), title='Добавление статьи')
+
+
+@app.route("/post/<int:id_post>")
+def showPost(id_post):
+    db = get_db()
+    dbase = FDatabase(db)
+    title, post = dbase.getPost(id_post)
+    if not title:
+        abort(404)
+
+    return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
 
 
 if __name__ == '__main__':
